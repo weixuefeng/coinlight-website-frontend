a25bc3d103cd49ea3712b0859b3a90e4010a6f82
arisac <hi@aris.ac>
2022-03-09 22:45:51 -0800

    add deps: next-themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
-  // darkMode: 'class', // 'media' is the default, change to 'class' if you want to use dark mode in with class names
+  darkMode: 'class', // 'media' is the default, change to 'class' if you want to use dark mode in with class names
   theme: {
     extend: {},
   },
