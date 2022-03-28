eeb69c5069a0e318d366291370292fb4d627bb9f
arisac <hi@aris.ac>
2022-03-28 00:08:29 -0700

    add `prose` class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,17 @@ function Header() {
 }
 
 function Main() {
-  return <main className={styles.main}>body</main>
+  return (
+    <main className={styles.main}>
+      <article className="prose dark:prose-invert md:prose-lg lg:prose-xl">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+      </article>
+    </main>
+  )
 }
 
 function Footer() {
