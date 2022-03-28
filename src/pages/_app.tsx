b9757b28e76c5f8299b8ad30e02332f54c727bdf
arisac <hi@aris.ac>
2022-03-28 06:34:24 -0700

    add defaultTheme="system"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider defaultTheme="system" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   )
