7ce860c22f7141f585570e54463aaee691d1ea75
liukeke <liukeke@diynova.com>
2022-10-24 19:52:41 +0800

    fix: bug

--- src/components/listContent.tsx
+++ src/components/listContent.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-24 18:07:49
+ * @LastEditTime: 2022-10-24 19:51:02
  * @FilePath: /coinlight/coinlight-website-frontend/src/components/header/header.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,7 @@ import { getRequest } from 'services/getAxios'
 
 export default function ListContent(props) {
   const listHeight = useRef<HTMLDivElement | null>(null)
-  const [total, setTotal] = useState(1)
+  const [total, setTotal] = useState<any>()
   const [page, setPage] = useState(1)
   const [newsData, setNewsData] = useState<any>([])
   const [isBootm, setIsBootm] = useState(false)
@@ -39,7 +39,7 @@ export default function ListContent(props) {
     return () => window.removeEventListener('scroll', handleScroll)
   })
 
-  const handleScroll = (e: any) => {
+  const handleScroll = () => {
     let scrollTop = document.documentElement.scrollTop
     let clientHeight = document.documentElement.clientHeight
     let scrollHeight = document.documentElement.scrollHeight
