fd5dd1649adb9e8de2eacd96c59f7ebe54597816
liukeke <liukeke@diynova.com>
2022-10-24 18:12:56 +0800

    fix: style css

--- src/components/listContent.tsx
+++ src/components/listContent.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-24 17:35:30
+ * @LastEditTime: 2022-10-24 18:07:49
  * @FilePath: /coinlight/coinlight-website-frontend/src/components/header/header.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,13 +22,11 @@ export default function ListContent(props) {
     const newsUrl = `api/proxy?news?populate=*&pagination[page]=${newPage}&pagination[pageSize]=10`
     const resNews = await getRequest(newsUrl)
     let data = resNews.data.data
-    console.log(newPage)
     if (newPage == 1) {
       setNewsData(data)
     } else {
       setNewsData(newsData.concat(data))
     }
-    console.log('newsData', newsData)
     setTotal(resNews.data.meta.pagination.total)
   }
 
