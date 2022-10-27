3285a452f504118ac46cf1046f6be4e3a875af1d
liukeke <liukeke@diynova.com>
2022-10-27 20:00:10 +0800

    fix: css style

--- src/components/listContent.tsx
+++ src/components/listContent.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
- * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-10-27 18:26:58
+ * @LastEditors: liukeke liukeke@diynova.com
+ * @LastEditTime: 2022-10-27 19:01:30
  * @FilePath: /coinlight-website-frontend/src/components/listContent.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -93,7 +93,7 @@ export default function ListContent(props) {
       </div>
       {newsData.length > 9 ? (
         <div className="date-loading">
-          <span onClick={getMoreDate}>{newsData.length < total ? '加载更多～' : '到底啦～'}</span>
+          {newsData.length < total ? <span onClick={getMoreDate}>加载更多</span> : <p>到底了</p>}
         </div>
       ) : null}
     </div>
--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 17:32:03
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-27 15:07:04
+ * @LastEditTime: 2022-10-27 19:30:51
  * @FilePath: /coinlight/coinlight-website-frontend/src/pages/details.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE<
  */
@@ -18,7 +18,7 @@ import { Skeleton } from 'antd'
 export default Home
 
 function Home() {
-  let pageModel = new PageModel('Detail', '', '')
+  let pageModel = new PageModel('Detail', '', 'detail-box')
   return <>{NormalLayout(Main(), pageModel)}</>
 }
 
