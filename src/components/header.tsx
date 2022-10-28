a8169bc90b610d54b8ecbf31200592a64412faa6
liukeke <liukeke@diynova.com>
2022-10-28 10:45:36 +0800

    fix: logo link and css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-17 14:08:25
+ * @LastEditTime: 2022-10-28 10:06:26
  * @FilePath: /coinlight/coinlight-website-frontend/src/components/header/header.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,7 +29,11 @@ export default function Header(props) {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/assets/image/logo.png" alt="logo" />
+        <Link href="/" passHref>
+          <div>
+            <img src="/assets/image/logo.png" alt="coinlight" />
+          </div>
+        </Link>
       </div>
       {/* <dl className={'language'} id="language">
           <dt>切换语言</dt>
