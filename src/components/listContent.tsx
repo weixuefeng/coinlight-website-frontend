cca758070949a1df0f66940f00e684ab6196eaaf
liukeke <liukeke@diynova.com>
2022-10-31 13:54:09 +0800

    fix: bug

--- src/components/listContent.tsx
+++ src/components/listContent.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-31 11:50:40
+ * @LastEditTime: 2022-10-31 13:50:13
  * @FilePath: /coinlight-website-frontend/src/components/listContent.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
