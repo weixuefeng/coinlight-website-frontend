c30e22e82c0f5ec7f525dc05812ce84739a9aca3
liukeke <liukeke@diynova.com>
2022-11-01 10:51:28 +0800

    fix: footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-13 11:26:10
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-17 15:39:08
+ * @LastEditTime: 2022-11-01 10:19:07
  * @FilePath: /nextjs-starter-boilerplate/src/components/footer/footer.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,7 +23,7 @@ export default function Footer() {
             Throw error
           </button>
         </div> */}
-        <p>Coinlight Copyright</p>
+        <p>Copyright © 2022 Coinlight</p>
       </div>
     </>
   )
