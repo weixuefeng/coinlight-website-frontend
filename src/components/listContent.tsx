a27f48bbd1e789396c7287b2d339a4adc6af507b
liukeke <liukeke@diynova.com>
2022-11-01 16:50:53 +0800

    fix: data

--- src/components/listContent.tsx
+++ src/components/listContent.tsx
@@ -2,7 +2,7 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-31 13:50:13
+ * @LastEditTime: 2022-11-01 16:46:12
  * @FilePath: /coinlight-website-frontend/src/components/listContent.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,9 +22,12 @@ export default function ListContent(props) {
   const [resData, setResData] = useState<any>({})
   const token = getLocalData('language', 'zh')
 
+  const [isLoading, setIsLoading] = useState(true)
+
   const getDataInfo = async languageVal => {
     const newsUrl = `api/proxy?news?filters[language]=${languageVal}&populate=*`
     const resNews = await getRequest(newsUrl)
+    setIsLoading(false)
     let data = resNews.data.data
     setTotal(data.length)
     data.sort(function (a, b) {
@@ -45,57 +48,59 @@ export default function ListContent(props) {
 
   useEffect(() => {
     getDataInfo(token)
-  }, [languageVal])
+  }, [languageVal, token])
+
+  if (isLoading) {
+    return arrLodaing.map(index => {
+      return (
+        <div className="content-lading" key={index}>
+          <Skeleton active paragraph={{ rows: 6 }} />
+        </div>
+      )
+    })
+  }
+
+  if (!newsData) {
+    return <div className="empty">Empty Data</div>
+  }
 
   return (
     <div className="list-content">
       <div className="list">
-        {newsData.length !== 0 ? (
-          newsData?.map((item, index) => {
-            return (
-              <a href={`/detail/${item.id}`} key={index}>
-                <div className="content-text">
-                  <h3>{item.attributes.title}</h3>
-                  <div className="content-info content-info-h5">
-                    <p>{item.attributes.content}</p>
-                  </div>
-                  <ul className="tags">
-                    {item.attributes.tags.data?.map((tagItem, i) => {
-                      return <li key={i}>{tagItem.attributes.name}</li>
-                    })}
-                  </ul>
-                  <div className="source-box">
-                    <div className="source">
-                      <img src={item.attributes.source_img} alt="img" />
-                      {item.attributes.source}
-                    </div>
-                    <div className="time">
-                      <i></i>
-                      {utcDateTime(item.attributes.publishedAt)}
-                    </div>
-                  </div>
-                </div>
-                <div className="content-info content-info-pc">
+        {newsData?.map((item, index) => {
+          return (
+            <a href={`/detail/${item.id}`} key={index}>
+              <div className="content-text">
+                <h3>{item.attributes.title}</h3>
+                <div className="content-info content-info-h5">
                   <p>{item.attributes.content}</p>
                 </div>
-              </a>
-            )
-          })
-        ) : (
-          <>
-            {arrLodaing.map(index => {
-              return (
-                <div className="content-lading" key={index}>
-                  <Skeleton active paragraph={{ rows: 6 }} />
+                <ul className="tags">
+                  {item.attributes.tags.data?.map((tagItem, i) => {
+                    return <li key={i}>{tagItem.attributes.name}</li>
+                  })}
+                </ul>
+                <div className="source-box">
+                  <div className="source">
+                    <img src={item.attributes.source_img} alt="img" />
+                    {item.attributes.source}
+                  </div>
+                  <div className="time">
+                    <i></i>
+                    {utcDateTime(item.attributes.publishedAt)}
+                  </div>
                 </div>
-              )
-            })}
-          </>
-        )}
+              </div>
+              <div className="content-info content-info-pc">
+                <p>{item.attributes.content}</p>
+              </div>
+            </a>
+          )
+        })}
       </div>
-      {newsData.length > 9 ? (
+      {newsData?.length > 9 ? (
         <div className="date-loading">
-          {newsData.length < total ? <span onClick={getMoreDate}>加载更多</span> : <p>到底了</p>}
+          {newsData?.length < total ? <span onClick={getMoreDate}>加载更多</span> : <p>到底了</p>}
         </div>
       ) : null}
     </div>
