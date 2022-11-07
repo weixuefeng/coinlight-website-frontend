6831e9e8afe715602b5413a60d44705c3b5d02a9
liukeke <liukeke@diynova.com>
2022-11-07 13:27:30 +0800

    fix: language

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,11 @@
  * @Author: liukeke liukeke@diynova.com
  * @Date: 2022-10-17 11:14:29
  * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-11-01 15:24:52
+ * @LastEditTime: 2022-11-07 13:20:36
  * @FilePath: /coinlight/coinlight-website-frontend/src/components/header/header.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useTranslation } from 'react-i18next'
 import { putLocalData } from 'localstorage/localstorage'
